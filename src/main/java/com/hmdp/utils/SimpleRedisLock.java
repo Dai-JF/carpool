16ea05af13402ce3b274aff7f077521a2e58708f
Dai-JF <dcd09@outlook.com>
2023-06-28 15:23:19 +0800

    实现redis分布式锁版本1

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,60 +1,45 @@
 package com.hmdp.utils;
 
-import cn.hutool.core.lang.UUID;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.core.script.DefaultRedisScript;
 
-import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
 
-    private String name;
-    private StringRedisTemplate stringRedisTemplate;
+    /**
+     * 锁的名称[业务名称]
+     */
+    private final String name;
+    private final StringRedisTemplate stringRedisTemplate;
+    private static final String KEY_PREFIX = "lock:";
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
         this.stringRedisTemplate = stringRedisTemplate;
     }
 
-    private static final String KEY_PREFIX = "lock:";
-    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
-    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
-    static {
-        UNLOCK_SCRIPT = new DefaultRedisScript<>();
-        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
-        UNLOCK_SCRIPT.setResultType(Long.class);
-    }
-
+    /**
+     * 尝试获取锁
+     *
+     * @param timeoutSec 锁持有的超时时间，过期后自动释放
+     * @return true代表获取锁成功; false代表获取锁失败
+     */
     @Override
     public boolean tryLock(long timeoutSec) {
-        // 获取线程标示
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 获取线程标识
+        long threadId = Thread.currentThread().getId();
+
         // 获取锁
         Boolean success = stringRedisTemplate.opsForValue()
-                .setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
+                .setIfAbsent(KEY_PREFIX + name, threadId + "", timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);
     }
 
+    /**
+     * 释放锁
+     */
     @Override
     public void unlock() {
-        // 调用lua脚本
-        stringRedisTemplate.execute(
-                UNLOCK_SCRIPT,
-                Collections.singletonList(KEY_PREFIX + name),
-                ID_PREFIX + Thread.currentThread().getId());
+        stringRedisTemplate.delete(KEY_PREFIX + name);
     }
-    /*@Override
-    public void unlock() {
-        // 获取线程标示
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        // 获取锁中的标示
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        // 判断标示是否一致
-        if(threadId.equals(id)) {
-            // 释放锁
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
-    }*/
 }
