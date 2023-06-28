6322b3fd556f554588c1e78447e9686c6656cfe2
Dai-JF <dcd09@outlook.com>
2023-06-28 16:49:36 +0800

    lua脚本保证判断锁和删除锁的原子性

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
@@ -19,6 +22,17 @@ public class SimpleRedisLock implements ILock {
      * 线程标识前缀[用于判断是否属于自己线程的锁，防止业务时间超过锁自动释放时间，导致锁误删]
      */
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    /**
+     * lua脚本
+     */
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
+
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -46,7 +60,13 @@ public class SimpleRedisLock implements ILock {
      */
     @Override
     public void unlock() {
-        // 获取线程标识
+        // 调用Lua脚本【保证判断锁和删除锁的原子性】
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
+
+        /* // 获取线程标识
         String threadId = ID_PREFIX + Thread.currentThread().getId();
 
         // 获取锁中的标识
@@ -55,8 +75,7 @@ public class SimpleRedisLock implements ILock {
         // 判断标识是否一致
         if (threadId.equals(id)) {
             // 释放锁
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
-
+            stringRedisTemplate.delete(KEY_PREFIX + name); */
+        // }
     }
 }
